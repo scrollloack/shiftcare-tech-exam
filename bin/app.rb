65f4aa26d556e92fe30447a79d6620830263323c
scrollloack <43851010+scrollloack@users.noreply.github.com>
2026-03-28 18:50:23 +0800

    Add rake tasks for automation testing

--- bin/app.rb
+++ bin/app.rb
@@ -5,7 +5,23 @@ require_relative '../app/services/find_similar_emails_service'
 options = CLI::MainOptionParser.new(ARGV).parse!
 
 input_file = ARGV.first
-json_data = JSON.parse(File.read(input_file))
+
+if input_file.nil?
+  puts "Error: No input file provided.\nUsage: ruby your_script.rb path/to/input.json [--search key=value] [--findSimilarEmails]"
+  exit(1)
+end
+
+unless File.exist?(input_file)
+  puts "Error: No such file or directory - #{input_file}"
+  exit(1)
+end
+
+begin
+  json_data = JSON.parse(File.read(input_file))
+rescue JSON::ParserError => e
+  puts "Error: Failed to parse JSON from file - #{e.message}"
+  exit(1)
+end
 
 result = if options[:find_similar_emails]
   Services::FindSimilarEmailsService.new(json_data)
